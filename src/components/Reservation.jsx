215a92d9f68033fcc2a8038d2a5b02cfef5d5577
amit979826 <aamitsingh821@gmail.com>
2026-01-20 14:30:21 +0530

    modifies backend

--- src/components/Reservation.jsx
+++ src/components/Reservation.jsx
@@ -1,9 +1,9 @@
-import React from 'react'
-import { HiOutlineArrowNarrowRight } from "react-icons/hi"
-import axios from "axios"
+import React from "react";
+import { HiOutlineArrowNarrowRight } from "react-icons/hi";
+import axios from "axios";
 import { useState } from "react";
 import toast from "react-hot-toast";
-import { useNavigate } from 'react-router-dom';
+import { useNavigate } from "react-router-dom";
 
 const Reservation = () => {
   const [firstName, setFirstName] = useState("");
@@ -11,15 +11,15 @@ const Reservation = () => {
   const [email, setEmail] = useState("");
   const [date, setDate] = useState("");
   const [time, setTime] = useState("");
-  const [phone, setPhone] = useState("");
-
+  const [phone, setPhone] = useState(0);
   const navigate = useNavigate();
 
   const handleReservation = async (e) => {
     e.preventDefault();
     try {
-      const { data } = await axios.post("https://restaurant-backend-neon.vercel.app/api/v1/reservation/send", 
-        { firstName, lastName, email, date, time, phone },
+      const { data } = await axios.post(
+        "http://localhost:4000/api/v1/reservation/send",
+        { firstName, lastName, email, phone, date, time },
         {
           headers: {
             "Content-Type": "application/json",
@@ -28,63 +28,84 @@ const Reservation = () => {
         }
       );
       toast.success(data.message);
-      setFirstName(""); 
+      setFirstName("");
       setLastName("");
-      setEmail("");
-      setDate("");
-      setTime("");
       setPhone(0);
-
-      navigate("/success")      
+      setEmail("");
+      setTime("");
+      setDate("");
+      navigate("/success");
     } catch (error) {
-        toast.error(error.response.data.message);
+      toast.error(error.response.data.message);
     }
-  }
+  };
 
-  return <section className="reservation" id="reservation">
+  return (
+    <section className="reservation" id="reservation">
       <div className="container">
-
         <div className="banner">
-            <img src="/reservation.png" alt="res"/>
+          <img src="/reservation.png" alt="res" />
         </div>
-        
         <div className="banner">
           <div className="reservation_form_box">
             <h1>MAKE A RESERVATION</h1>
             <p>For Further Questions, Please Call</p>
-
             <form>
-
-                  <div>
-                      <input type="text" 
-                      placeholder="First Name" 
-                      value={firstName} 
-                      onChange={(e)=>setFirstName(e.target.value)}/>
-                      <input type="text" 
-                      placeholder="Last Name" 
-                      value={lastName} 
-                      onChange={(e)=>setLastName(e.target.value)}/>
-                  </div>
-
-                  <div>
-                      <input type="date" placeholder='Date' value={date} onChange={(e)=>setDate(e.target.value)}/>
-                      <input type="time" placeholder='Time' value={time} onChange={(e)=>setTime(e.target.value)}/>
-                  </div>
-
-                  <div>
-                      <input type="email" placeholder='Email' className="email_tag" value={email} onChange={(e)=>setEmail(e.target.value)}/>
-                      <input type="number" placeholder='phone' value={phone} onChange={(e)=>setPhone(e.target.value)}/>
-                  </div>
-
-                  <button type="submit" onClick={handleReservation}>RESERVE NOW {" "} <span>
-                    <HiOutlineArrowNarrowRight/>
-                  </span></button>
+              <div>
+                <input
+                  type="text"
+                  placeholder="First Name"
+                  value={firstName}
+                  onChange={(e) => setFirstName(e.target.value)}
+                />
+                <input
+                  type="text"
+                  placeholder="Last Name"
+                  value={lastName}
+                  onChange={(e) => setLastName(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="date"
+                  placeholder="Date"
+                  value={date}
+                  onChange={(e) => setDate(e.target.value)}
+                />
+                <input
+                  type="time"
+                  placeholder="Time"
+                  value={time}
+                  onChange={(e) => setTime(e.target.value)}
+                />
+              </div>
+              <div>
+                <input
+                  type="email"
+                  placeholder="Email"
+                  className="email_tag"
+                  value={email}
+                  onChange={(e) => setEmail(e.target.value)}
+                />
+                <input
+                  type="number"
+                  placeholder="Phone"
+                  value={phone}
+                  onChange={(e) => setPhone(e.target.value)}
+                />
+              </div>
+              <button type="submit" onClick={handleReservation}>
+                RESERVE NOW{" "}
+                <span>
+                  <HiOutlineArrowNarrowRight />
+                </span>
+              </button>
             </form>
           </div>
         </div>
       </div>
-  </section>
-  
-}
+    </section>
+  );
+};
 
-export default Reservation
+export default Reservation;
